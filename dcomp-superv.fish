9d73a9fede52771e2814700f0b7ee41f1f857013
Lars Claussen <lars.claussen@nelen-schuurmans.nl>
2017-06-12 10:56:19 +0200

    indentation

--- dcomp-superv.fish
+++ dcomp-superv.fish
@@ -5,7 +5,7 @@ function dcomp-superv
     if docker-compose exec $argv[1] echo "Running..."
         set_color normal
         docker-compose exec $argv[1] bin/supervisorctl $argv[2..-1]
-	else
+    else
         set_color red
         echo "Container is not running. You probably have to execute 'docker-compose up [args]' first"
     end
